a97cd18911b09c58129b3668af84f1c4b0197149
Franz Lehmann <120375184+lepeitschke@users.noreply.github.com>
2024-12-25 21:31:57 +0000

    Automatic commit before Assistant edits

---
+++ Day-001/Day-1-band-name-again.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator!")
+
+town = input("What is the name of the town you grew up in?\n")
+
+pet = input("What is the name of your pet?\n")
+
+print("Your band name could be " + town + " " + pet)
